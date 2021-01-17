5de666ded99f590bcc34400121935c7e1867cc05
Jaime Lannister <jaime.kings.7@gmail.com>
2021-01-17 21:19:39 +0800

    Updates

--- graph.js
+++ graph.js
@@ -278,14 +278,14 @@ $(function(){
     changeMonth: true,
     changeYear: true,
     constrainInput: true,
-    yearRange: '1986:2020'
+    yearRange: '1986:2021'
   });
   $('#endDate').datepicker({
     dateFormat: 'yy-mm-dd',
     changeMonth: true,
     changeYear: true,
     constrainInput: true,
-    yearRange: '1986:2020'
+    yearRange: '1986:2021'
   });
   $('#showStartDate').change(function(){
     $.graph.setupGrid();
